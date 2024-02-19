9409582837d42c2cfdd37de5e83d8d13ff286421
ArunKarthik05 <Kevinsanjaya@5>
2024-02-19 12:25:34 +0530

    halfway

--- script.mjs
+++ script.mjs
@@ -1,3 +1,22 @@
+//-cursor
+const dot = document.querySelector(".cursor-dot");
+const circle = document.querySelector(".cursor-outline");
+
+window.addEventListener("mousemove",(e)=>{
+    const posX  = e.clientX
+    const posY  = e.clientY
+
+    dot.style.left = `${posX}px`
+    circle.style.left = `${posX}px`
+
+    dot.style.top = `${posY}px`
+    circle.style.top = `${posY}px`
+
+    circle.animate({
+        top:`${posY}px`,
+        left : `${posX}px`
+    },{duration:500, fill:"forwards"})
+})
 // ----------------Navbar-appear----------
 const navbar = document.getElementById("float-nav")
 
@@ -18,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const slider = document.querySelector('.slider');
     let observerAdded = false;
     
-    // Options for the intersection observer
     const options = {
         root: null,
         rootMargin: '0px',
@@ -31,7 +49,6 @@ document.addEventListener("DOMContentLoaded", function() {
         entries.forEach(entry => {
             if (entry.isIntersecting && !observerAdded) {
                 observerAdded= true;
-                console.log("starting")
                 startSlideAnimation();
             }
         });
@@ -89,7 +106,8 @@ const model = document.querySelector(".car");
 let images = ["black","pink","yellow","green","blue"];
 let imgContainer = document.querySelector(".img-clr-container");
 let img = document.getElementById("colors-img");
-
+const spantxt = document.querySelector(".colors-text")
+console.log(spantxt)
 
 colorOptions.forEach((colorOption,i) => {
     colorOption.addEventListener('click', function() {
@@ -107,12 +125,96 @@ colorOptions.forEach((colorOption,i) => {
         if (i === 0) {
             model.style.display = "block";
             imgContainer.style.display = "none";
+            spantxt.style.bottom = "120px"
         } else {
             model.style.display = "none";
             imgContainer.style.display = "block";
             let path = `./assets/phone-colors/${images[i]}.jpg`
-            console.log(path)
-            img.src = path; // Use the corresponding image URL
+            img.src = path; 
+            spantxt.textContent = `15.54 cm (6.1”) iPhone 15 in ${images[i]}`
+            spantxt.style.bottom = "180px"
+            console.log(spantxt.textContent)
         }
     });
 });
+// ------------------------Floating Bottom--------------
+const colorsContainer = document.querySelector('.colors');
+
+window.addEventListener('scroll', function() {
+    const section = document.querySelector('.model');
+    const sectionTop = section.getBoundingClientRect().top+500;
+    // const windowHeight = window.innerHeight;
+
+    if ( sectionTop > 300 && sectionTop>0) {
+        colorsContainer.style.visibility = "visible";
+        colorsContainer.classList.remove("hidden");
+    } else {
+        colorsContainer.style.visibility = "hidden";
+        colorsContainer.classList.add("hidden");
+    }
+});
+// ---------------Potrait Slider----------
+// const sliderContainer = document.querySelector('.potrait-slider-container');
+
+// let focusedIndex = 0;
+
+// function updateSlider() {
+//     const items = document.querySelectorAll('.slider-item');
+//     items.forEach((item, index) => {
+//         if (index === focusedIndex) {
+//             item.classList.add('focused');
+//             item.classList.remove('overlay');
+//         } else {
+//             item.classList.remove('focused');
+//             item.classList.add('overlay');
+//         }
+//     });
+// }
+
+// function handleSwipe(direction) {
+//     if (direction === 'right') {
+//         focusedIndex = (focusedIndex + 1) % items.length;
+//     } else if (direction === 'left') {
+//         focusedIndex = (focusedIndex - 1 + items.length) % items.length;
+//     }
+//     updateSlider();
+// }
+
+// sliderContainer.addEventListener('touchstart', handleTouchStart, false);
+// sliderContainer.addEventListener('touchmove', handleTouchMove, false);
+
+// let xDown = null;
+// let yDown = null;
+
+// function handleTouchStart(event) {
+//     const firstTouch = event.touches[0];
+//     xDown = firstTouch.clientX;
+//     yDown = firstTouch.clientY;
+// }
+
+// function handleTouchMove(event) {
+//     if (!xDown || !yDown) {
+//         return;
+//     }
+
+//     const xUp = event.touches[0].clientX;
+//     const yUp = event.touches[0].clientY;
+
+//     const xDiff = xDown - xUp;
+//     const yDiff = yDown - yUp;
+
+//     if (Math.abs(xDiff) > Math.abs(yDiff)) {
+//         /* Most significant */
+//         if (xDiff > 0) {
+//             /* left swipe */
+//             handleSwipe('left');
+//         } else {
+//             /* right swipe */
+//             handleSwipe('right');
+//         }
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;
+// }
+// -----------Modern---------------
